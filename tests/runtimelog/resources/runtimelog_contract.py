55869398e35c0b10921bfbfc6a5046fb49df24e6
Lucas Kenji Uezu <49196318+luc10921@users.noreply.github.com>
2025-12-24 06:15:11 -0300

    node: fix runtime log capturing (#20)
    * node: decode unicode escapes and fix regex not catching lists
    
    (cherry picked from commit 23e01649ed2c414d727c555687c2dcdd17c68867)
    
    * case: explicitly allow None as return type
    
    (cherry picked from commit ecb8ee15e17397096837ef69465fa048c2556483)
    
    * fix lint
    
    * fix msg capturing
    
    * remove type annotation change
    
    ---------
    
    Co-authored-by: Erik van den Brink <git@erikvandenbrink.com>

--- tests/runtimelog/resources/runtimelog_contract.py
+++ tests/runtimelog/resources/runtimelog_contract.py
@@ -2,5 +2,13 @@ from boa3.sc.compiletime import public
 
 
 @public
-def main(msg: str):
+def print_str(msg: str):
+    print(msg)
+
+@public
+def print_bytes(msg: bytes):
+    print(msg)
+
+@public
+def print_list(msg: list):
     print(msg)
